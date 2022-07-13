8ec24d6ee112d7f9acf2a9e3a46801dc092c5e37
EZEN-133 <EZEN-133@DESKTOP-CVEHFNM>
2022-07-13 15:57:04 +0900

    SDY

---
+++ SDY/src/Sdy.java
@@ -0,0 +1,15 @@
+
+public class Sdy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("OGH  KMJ  KSJ");
+		System.out.println("KJS  KTH  NKH");
+		System.out.println("LDH  PYM  ABG");
+		System.out.println("CEH  LYB  LJH");
+		System.out.println("CYS  CHS  HWJ ");
+		
+	}
+
+}
